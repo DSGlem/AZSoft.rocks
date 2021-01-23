2afb7dcad3cddcfdf0756ff3b377dc0ce6d31a9e
DSGlem <davidsporysh@gmail.com>
2021-01-23 19:17:39 +0200

    pages: about, apps, back-end, design, front-end

---
+++ pages/about.vue
@@ -0,0 +1,29 @@
+<template>
+  <main>
+    <BaseHero v-bind="hero"></BaseHero>
+  </main>
+</template>
+
+<script>
+export default {
+  layout: "default",
+  data() {
+    return {
+      hero: {
+        title: "About us",
+        description:
+          "Though we’re experts with a proven track record and a great reputation for helping companies do business faster, better and smarter than before, we never let ourselves forget that we’re fortunate to have our clients and not the other way around.",
+        link: null,
+        image: {
+          src: require("~/assets/images/about/hero.png"),
+          alt: "notebook image",
+        },
+        background: {
+          image: require("@/assets/images/about/hero-bg.jpg"),
+          color: "#000",
+        },
+      },
+    };
+  },
+};
+</script>
---
+++ pages/front-end.vue
@@ -0,0 +1,25 @@
+<template>
+  <main>
+    <BaseHero v-bind="hero"></BaseHero>
+  </main>
+</template>
+
+<script>
+export default {
+  layout: "default",
+  data() {
+    return {
+      hero: {
+        title: "Backend Development",
+        description:
+          "Custom software products and services are our specialty, and we will ensure that your project receives the highest level of attention and customer care. Our job isn't done until you're satisfied with your online presence. For more details, don't wait!",
+        link: { name: "See cases", to: "/" },
+        image: {
+          src: require("~/assets/images/front-end/hero.png"),
+          alt: "notebook image",
+        },
+      },
+    };
+  },
+};
+</script>
